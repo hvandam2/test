2daa79417ab19e501e9b53181bfb667edb6739b3
Howard Van Dam <hvandam2@gmail.com>
2025-04-09 14:54:51 -0600

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,8 @@
+#
+# a python file
+# 
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+total = first_number + second_number
+print("The sum of the numbers is:", total)
+
